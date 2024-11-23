80039a9bb5f5c137488dd67351ca966b822cd41c
Zohir <vasv29834@gmail.com>
2024-11-23 22:10:14 +0500

    feat: tailwind test

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="fixed top-0 left-0 w-full h-16 border-b-1 border-b-white"></div>
+    );
+}
+
+export default Header;
