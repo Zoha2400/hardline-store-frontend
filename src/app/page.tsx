ef781fb50e522352fae8a3fab275edf27753a12a
Zohir <vasv29834@gmail.com>
2024-11-29 23:11:53 +0500

    feat: frontend gets all data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,80 @@
+'use client'
+
+import {useEffect, useState} from 'react'
 import Dragon from "@/components/Header/Dragon";
-import Breadcrumbs from "@/components/Header/Breadcrupms";
 import Card from "@/components/Cards/Card";
 import SerachLine from "@/components/Cards/SerachLine";
 import Filter from "@/components/Cards/Filter";
 
-const cards = [
-    {
-        img: '/example.svg',
-        rate: 4.5,
-        title: 'MSI X344',
-        price: 1.499,
-        description: 'Intel Core i9-11900KF, 16GB DDR4, 512GB SSD, NVIDIA GeForce RTX 3060 Ti, 165W, 100% UEFI, 144Hz, 1080p',
-        id: '184329',
-        url: '/product/184329',
-    },
-    {
-        img: '/example.svg',
-        rate: 4.5,
-        title: 'MSI X344',
-        price: 1.499,
-        description: 'Intel Core i9-11900KF, 16GB DDR4, 512GB SSD, NVIDIA GeForce RTX 3060 Ti, 165W, 100% UEFI, 144Hz, 1080p',
-        id: '184329',
-        url: '/product/184329',
-    },
-    {
-        img: '/example.svg',
-        rate: 4.5,
-        title: 'MSI X344',
-        price: 1.499,
-        description: 'Intel Core i9-11900KF, 16GB DDR4, 512GB SSD, NVIDIA GeForce RTX 3060 Ti, 165W, 100% UEFI, 144Hz, 1080p',
-        id: '184329',
-        url: '/product/184329',
-    },
-    {
-        img: '/example.svg',
-        rate: 4.5,
-        title: 'MSI X344',
-        price: 1.499,
-        description: 'Intel Core i9-11900KF, 16GB DDR4, 512GB SSD, NVIDIA GeForce RTX 3060 Ti, 165W, 100% UEFI, 144Hz, 1080p',
-        id: '184329',
-        url: '/product/184329',
-    },
-    {
-        img: '/example.svg',
-        rate: 4.5,
-        title: 'MSI X344',
-        price: 1.499,
-        description: 'Intel Core i9-11900KF, 16GB DDR4, 512GB SSD, NVIDIA GeForce RTX 3060 Ti, 165W, 100% UEFI, 144Hz, 1080p',
-        id: '184329',
-        url: '/product/184329',
-    },
-]
-export default function Home() {
+import axios from 'axios';
+
+
+// const cards = [
+//     {
+//         img: '/example.svg',
+//         rate: 4.5,
+//         title: 'MSI X344',
+//         price: 1.499,
+//         description: 'Intel Core i9-11900KF, 16GB DDR4, 512GB SSD, NVIDIA GeForce RTX 3060 Ti, 165W, 100% UEFI, 144Hz, 1080p',
+//         id: '184329',
+//         url: '/product/184329',
+//     },
+//     {
+//         img: '/example.svg',
+//         rate: 4.5,
+//         title: 'MSI X344',
+//         price: 1.499,
+//         description: 'Intel Core i9-11900KF, 16GB DDR4, 512GB SSD, NVIDIA GeForce RTX 3060 Ti, 165W, 100% UEFI, 144Hz, 1080p',
+//         id: '184329',
+//         url: '/product/184329',
+//     },
+//     {
+//         img: '/example.svg',
+//         rate: 4.5,
+//         title: 'MSI X344',
+//         price: 1.499,
+//         description: 'Intel Core i9-11900KF, 16GB DDR4, 512GB SSD, NVIDIA GeForce RTX 3060 Ti, 165W, 100% UEFI, 144Hz, 1080p',
+//         id: '184329',
+//         url: '/product/184329',
+//     },
+//     {
+//         img: '/example.svg',
+//         rate: 4.5,
+//         title: 'MSI X344',
+//         price: 1.499,
+//         description: 'Intel Core i9-11900KF, 16GB DDR4, 512GB SSD, NVIDIA GeForce RTX 3060 Ti, 165W, 100% UEFI, 144Hz, 1080p',
+//         id: '184329',
+//         url: '/product/184329',
+//     },
+//     {
+//         img: '/example.svg',
+//         rate: 4.5,
+//         title: 'MSI X344',
+//         price: 1.499,
+//         description: 'Intel Core i9-11900KF, 16GB DDR4, 512GB SSD, NVIDIA GeForce RTX 3060 Ti, 165W, 100% UEFI, 144Hz, 1080p',
+//         id: '184329',
+//         url: '/product/184329',
+//     },
+// ]
+export default  function Home ()  {
+
+    const [cards, setCards] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8000/get_all');
+                console.log(response.data);
+                // @ts-ignore
+                setCards(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
   return (
     <div className="container flex justify-center">
         <div className="w-11/12">
@@ -72,4 +95,4 @@ export default function Home() {
             </div>
         </div>
         );
-        }
+}
--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 function Card({data}: any) {
     return (
-        <Link href={data.url}>
+        <Link href={`/product/${data.product_id}`}>
             <div
                 className="w-64 h-100 bg-neutral-800 flex flex-col rounded-2xl overflow-hidden justify-center items-center hover:bg-neutral-700 cursor-pointer duration-200 group">
                 <div className="overflow-hidden w-full">
@@ -22,7 +22,7 @@ function Card({data}: any) {
 
                 <div className="p-3 flex flex-col gap-2">
                     <p className="text-xl font-bold p-2gi">
-                        {data.title}
+                        {data.product_name}
                     </p>
                     <div className="w-full rating flex text-yellow-400">
                         <Icon icon="material-symbols:kid-star"/>
@@ -34,7 +34,7 @@ function Card({data}: any) {
                         <p className="text-xs px-2 text-gray-400">{data.rate} out of 5</p>
                     </div>
                     <p className="text-sm">
-                        {data.description}
+                        {data.product_description}
                     </p>
                     <p className="text-xl text-gray-200 font-bold">${data.price}</p>
                 </div>
