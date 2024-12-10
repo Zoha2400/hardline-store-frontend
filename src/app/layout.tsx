ca265ee260cf31edb3db8e1428e2fd2c39fa2cf4
Zohir <vasv29834@gmail.com>
2024-12-10 19:55:27 +0500

    fix: redirect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
+import Cookies from "js-cookie";
 
 const pagesWithoutLayout = [
   "auth",
@@ -15,12 +17,35 @@ const pagesWithoutLayout = [
   "redirect",
 ];
 
+const pagesWithRegistration = [
+  "admin",
+  "admin/users",
+  "admin/roles",
+  "admin/permissions",
+  "admin/logs",
+  "admin/settings",
+  "profile",
+  "profile/change",
+  "orders",
+];
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
+  const router = useRouter();
+  const email = Cookies.get("email");
+
+  useEffect(() => {
+    if (
+      pagesWithRegistration.some((page) => pathname.includes(page)) &&
+      !email
+    ) {
+      router.replace("/auth/reg");
+    }
+  }, []);
 
   const shouldUseLayout = !pagesWithoutLayout.some((page) =>
     pathname.includes(page),
@@ -32,7 +57,6 @@ export default function RootLayout({
         {shouldUseLayout ? (
           <>
             <Header />
-
             <main className="mt-28">{children}</main>
             <Footer />
           </>
